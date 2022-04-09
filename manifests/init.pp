62f5974b176547e350a4fe95e36a995611e7fb49
ottagit <ottaosawa@gmail.com>
2022-04-09 18:28:56 +0000

    add module manifest and config file

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# Manage NTP
+
+# Class:: chris_ntp
+
+class chris_ntp {
+  ensure_packages(['ntp'])
+
+  file {
+    "/etc/ntp.conf":
+      ensure  => 'file',
+      source  => 'puppet:///modules/chris_ntp/ntp.conf',
+      notify  => Service['ntp'],
+      require => Package['ntp'],
+  }
+
+  service {
+    "ntp":
+      ensure   => 'running',
+      enable   => 'true',
+  }
+}
